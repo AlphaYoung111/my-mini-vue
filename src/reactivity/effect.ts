2452a5b0183bdd084302fe9a32f3cc6a2f8da754
ayoung <729346015@qq.com>
2022-03-17 22:23:42 +0800

    feat:添加trackh函数收集依赖

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,3 +1,44 @@
+class ReactiveEffect {
+  private _fn:Function
+  constructor(fn:Function) {
+    this._fn = fn;
+  }
+
+  run () {
+    activeEffect = this
+    this._fn()
+  }
+}
+
+const targetMap = new WeakMap()
+// {
+//   obj: {
+//     key: []
+//   }
+// }
+export function track(target,key) {
+  // target => key => deps
+  let depsMap = targetMap.get(target) as WeakMap<object,WeakSet<Function>>
+  if (!depsMap) {
+    depsMap = new WeakMap()
+    targetMap.set(target,depsMap)
+  }
+  
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new WeakSet()
+    depsMap.set(key,dep) 
+  }
+
+  // 将当前执行得effect添加到对应得dep中
+  dep.add(activeEffect)
+  activeEffect = null
+}
+
+// 保存当前执行得effect
+let activeEffect
 export function effect (fn) {
-  fn()
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
 }
