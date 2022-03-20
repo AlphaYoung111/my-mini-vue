73645671da785fd6e939fcfcbfd2294566493827
ayoung <729346015@qq.com>
2022-03-20 19:02:30 +0800

    feat:trigger函数

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,10 +1,10 @@
 class ReactiveEffect {
-  private _fn:Function
-  constructor(fn:Function) {
+  private _fn: Function
+  constructor(fn: Function) {
     this._fn = fn;
   }
 
-  run () {
+  run() {
     activeEffect = this
     this._fn()
   }
@@ -16,37 +16,50 @@ const targetMap = new WeakMap()
 //     key: []
 //   }
 // }
-export function track(target,key) {
+export function track(target, key) {
   // target => key => deps
-  let depsMap = targetMap.get(target) as Map<PropertyKey,Set<Function>>
+  let depsMap = targetMap.get(target) as Map<PropertyKey, Set<ReactiveEffect>>
   if (!depsMap) {
     depsMap = new Map()
-    targetMap.set(target,depsMap)
+    targetMap.set(target, depsMap)
   }
-  
+
   let dep = depsMap.get(key)
   if (!dep) {
     dep = new Set()
-    depsMap.set(key,dep) 
+    depsMap.set(key, dep)
   }
 
   // 将当前执行得effect添加到对应得dep中
   dep.add(activeEffect)
   console.log('我收集了')
 
-  activeEffect = null
+  // activeEffect = null
 }
 
 
-export function trigger (target,key,value) {
+export function trigger(target, key, value) {
+  let depsMap = targetMap.get(target) as Map<PropertyKey, Set<ReactiveEffect>>
+  if (!depsMap) {
+    throw new Error(`not found ${target}`)
+  }
   
+  let dep = depsMap.get(key)
+  if (!dep) {
+    throw new Error(`not found ${key} in ${target}`)
+  }
+
+  for (const effect of dep) {
+    effect.run()
+  }
+
+
 }
 
 // 保存当前执行得effect
 let activeEffect
-export function effect (fn) {
+export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
-
   // 接收到同时，立马执行一次传进来得函数
   _effect.run()
 }
